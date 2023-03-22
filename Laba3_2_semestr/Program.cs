4e0548c64271d8ac6d053e03f1a189ebe7311334
Олександр Клименко <alklimenko227@outlook.com>
2023-03-22 20:21:16 +0200

    Добавьте файлы проекта.

---
+++ Laba3_2_semestr/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laba3_2_semestr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
